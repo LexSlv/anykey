81fd421a1b107ff43f6f15f702c3e1cc8ded47ad
a.slivnev <alexey.slivnev@gmail.com>
2019-04-26 17:36:04 +0600

    Пример миграции, CRUD новости, новый текстовый редактор

---
+++ models/News.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property string $image
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'image'], 'required'],
+            [['content'], 'string'],
+            [['title', 'image'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Заголовок',
+            'content' => 'Текст',
+            'image' => 'Картинка',
+        ];
+    }
+}
